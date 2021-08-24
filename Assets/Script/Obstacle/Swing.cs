5381c9006a64702a6f53842ef1d5ca005c9ccbef
kuro1307 <56852370+sondo1307@users.noreply.github.com>
2021-08-24 16:40:20 +0700

    Ninja_2-24/08

--- Assets/Script/Obstacle/Swing.cs
+++ Assets/Script/Obstacle/Swing.cs
@@ -11,9 +11,7 @@ public class Swing : MonoBehaviour
     public float defaultTimer;
     private int c;
     private Vector3 originTransform;
-    public CapsuleCollider child;
     public float destinationX;
-    public CapsuleCollider capsuleBoxMain;
 
     private void Start()
     {
@@ -24,17 +22,12 @@ public class Swing : MonoBehaviour
         timer -= Time.deltaTime;
         if (Vector3.Distance(transform.position, new Vector3(0,transform.position.y, transform.position.z)) >= originTransform.x && c==0 && timer <= 0)
         {
-            child.enabled = true;
-            capsuleBoxMain.isTrigger = true;
             transform.DOMoveX(destinationX, swingTime).SetEase(Ease.Linear);
             c = 1;
             timer = defaultTimer;
         }
         else if (Vector3.Distance(transform.position, new Vector3(destinationX, transform.position.y, transform.position.z)) <= 0.1f && c == 1)
         {
-            child.enabled = false;
-            capsuleBoxMain.isTrigger = false;
-
             transform.DOMove(originTransform, returnTime).SetEase(Ease.Linear);
             c = 0;
         }
